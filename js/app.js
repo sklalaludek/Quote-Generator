dcc6e8ea2e9b65f2fb53ef7e26eb42a2f57f9d98
sklalaludek <pattiegolis@gmail.com>
2017-05-09 21:08:20 +0200

    add quotes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-    console.log('chuck');
+
+    function generateQuotes(){
+        var btn = $("button");
+        var urlChuckNorrisRandom = "https://api.chucknorris.io/jokes/random";
+        var urlChuckNorrisGif = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris";
+
+        /* Insert Random Quotes to DOM  */
+          function getData(url){
+              $.ajax({
+                      url: url,
+                      method: "GET",
+                      dataType: "json"
+                  })
+                  .done(function(response) {
+                      $("#quotes").text(response.value);
+                  });
+          }
+
+          btn.on('click', function() {
+              getData(urlChuckNorrisRandom);
+          });
+    }
+    generateQuotes();
 });
