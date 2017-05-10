956a887992990d137be74a32c551b36050844879
sklalaludek <pattiegolis@gmail.com>
2017-05-10 17:19:39 +0200

    add gifs

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
 $(document).ready(function() {
 
-    function generateQuotes(){
+    function generateQuotes() {
         var btn = $("button");
         var urlChuckNorrisRandom = "https://api.chucknorris.io/jokes/random";
         var urlChuckNorrisGif = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris";
 
         /* Insert Random Quotes to DOM  */
-          function getData(url){
-              $.ajax({
-                      url: url,
-                      method: "GET",
-                      dataType: "json"
-                  })
-                  .done(function(response) {
-                      $("#quotes").text(response.value);
-                  });
-          }
-
-          btn.on('click', function() {
-              getData(urlChuckNorrisRandom);
-          });
+        function getData(url) {
+            $.ajax({
+                    url: url,
+                    method: "GET",
+                    dataType: "json"
+                })
+                .done(function(response) {
+                    $("#quotes").text(response.value);
+                });
+        }
+        
+        /* Insert Random Gifs to DOM  */
+        function getPic(url) {
+            $.ajax({
+                    url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris",
+                    method: "GET",
+                    dataType: "json"
+                })
+                .done(function(response) {
+                    console.log(response.data.image_url);
+                    $("#gifs").css("background-image", 'url("' + response.data.image_url + '")');
+                });
+        }
+        btn.on("click", function() {
+            getData(urlChuckNorrisRandom);
+            getPic(urlChuckNorrisGif);
+        });
     }
     generateQuotes();
 });
