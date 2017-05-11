caf66aea2247ae8a36639c065bf02f5611956e06
sklalaludek <pattiegolis@gmail.com>
2017-05-11 17:11:28 +0200

    remove console.log

--- js/app.js
+++ js/app.js
@@ -20,12 +20,11 @@ $(document).ready(function() {
         /* Insert Random Gifs to DOM  */
         function getPic(url) {
             $.ajax({
-                    url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=chuck+norris",
+                    url: url,
                     method: "GET",
                     dataType: "json"
                 })
                 .done(function(response) {
-                    console.log(response.data.image_url);
                     $("#gifs").css("background-image", 'url("' + response.data.image_url + '")');
                 });
         }
